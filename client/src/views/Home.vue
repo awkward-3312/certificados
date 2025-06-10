5ea2f02972c438bae092aa72d4d82279587e6a40
awkward-3312 <betza_es77@hotmail.com>
2025-06-09 20:07:08 -0600

    Delete client directory

--- client/src/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="p-4 text-center">
-    <h1 class="text-2xl font-bold mb-4">Certificados Regulatorios</h1>
-    <p class="text-gray-700">Herramienta para consultar y administrar la vigencia de certificados.</p>
-  </div>
-</template>
-
-<script setup>
-</script>
