1a1eb7a0216c221c4d827078feeaec7aea297ee2
awkward-3312 <betza_es77@hotmail.com>
2025-06-10 11:15:00 -0600

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,8 @@
-const supabaseUrl = '';
-const supabaseKey = '';
+const supabaseUrl = 'https://yzdjpwdoutjeuuvxmqmv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6ZGpwd2RvdXRqZXV1dnhtcW12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1MTg2NTMsImV4cCI6MjA2NTA5NDY1M30.WgC0o1VLVCMTi2cKiGC6OIPHrwgjTAav3DQ_k7JUGEg';
+
 window.supa = supabase.createClient(supabaseUrl, supabaseKey);
+
 window.ALLOWED_EMAILS = [
   'betza@certificate.com',
   'ingrid@certificate.com',
